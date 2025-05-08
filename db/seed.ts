8f15a254398d62061da625933092271d9b3be9de
Julio Chen <julio111777@hotmail.com>
2025-05-07 22:55:33 -0400

    second

--- db/seed.ts
+++ db/seed.ts
@@ -1,15 +1,15 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import sampleData from "./sample-data";
-// import { hash } from '@/lib/encrypt';
+// import { hash } from "@/lib/encrypt";
 
 async function main() {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
-  // await prisma.account.deleteMany();
-  // await prisma.session.deleteMany();
-  // await prisma.verificationToken.deleteMany();
-  // await prisma.user.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   await prisma.product.createMany({ data: sampleData.products });
   // const users = [];
@@ -23,7 +23,7 @@ async function main() {
   //     await hash(sampleData.users[i].password)
   //   );
   // }
-  // await prisma.user.createMany({ data: users });
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log("Database seeded successfully!");
 }
